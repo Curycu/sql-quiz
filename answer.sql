19c6bc9dc27bf33406134732724f3edce7544433
Ha, Hun Cheol <hancury@gmail.com>
2018-09-13 11:28:37 +0900

    Update answer.sql

--- answer.sql
+++ answer.sql
@@ -1,73 +1,76 @@
 # Q1) select, where
-# 모든 2017년 2월 이후 가입 고객의 id, status, signup을 표시해주세요
+# 2017년 2월 이후 가입 고객의 id, status, signup을 표시해주세요
 select 
-  id, status, signup
-from ba.interview_user
+  id, 
+  `status`, 
+  signup 
+from ba.interview_user 
 where signup >= '2017-02-01 00:00:00'
 ;
-
-# Q2) count, distinct, where
+ 
+# Q2) where, group by, count, distinct
 # 월별 도서구매고객수를 표시해주세요
-select
-  substring(pay_date, 1, 7) as `pay_month`, 
-  count(distinct user_id) as `user_count`
-from ba.interview_payment
-where pay_for = 'book'
-group by
-  substring(pay_date, 1, 7)
+select 
+  year(pay_date), 
+  month(pay_date), 
+  count(distinct user_id) as user_count 
+from interview_payment 
+where pay_for = 'book' 
+group by 
+  year(pay_date), 
+  month(pay_date)
 ;
-
-# Q3) group by, if, sum, left join
+ 
+# Q3) left join, group by, sum, if
 # 모든 고객별 id, 누적리디캐시충전액, 누적기기구매액을 표시해주세요
-select
-  u.id as `user_id`,
-  sum(if(p.pay_for = 'ridicash', p.price, 0)) as `ridicash_price`,
-  sum(if(p.pay_for = 'device', p.price, 0)) as `device_price`
+select 
+  u.id, 
+  sum(if(p.pay_for = 'ridicash', p.price, 0)) as ridicash, 
+  sum(if(p.pay_for = 'device', p.price, 0)) as device 
 from ba.interview_user u
-left join ba.interview_payment p on u.id = p.user_id
-group by
-  u.id
+left join ba.interview_payment p on u.id = p.user_id 
+group by u.id
 ;
-
-# Q4) where, group by, having, if, left join
-# 고객 status가 'active'이며 2017년 3월 이전 가입하였고 누적도서구매액이 1000원 이상인 경우 grade 'A' 그 외 고객은 grade 'B' 
-# 모든 고객에 대해 `user_id`, `signup`, `grade` 컬럼을 표시해주세요
-select
+ 
+# Q4) where, group by, if, left join
+# 고객 status가 'active'이며 2017년 3월 이전 가입하였고 누적도서구매액이 1000원 이상인 경우 grade 'A' 그 외 고객은 grade 'B'
+# 모든 고객에 대해 `user_id`, `signup`, `status`, `cummulative_price`, `grade` 컬럼을 표시해주세요
+select 
   u.id as `user_id`,
   u.signup,
-  if(g.grade = 'A', 'A', 'B') as `grade`
+  u.`status`,
+  p.price as `cummulative_price`,
+  (case 
+    when u.status = 'active' and u.signup < '2017-03-01 00:00:00' and p.price >= 1000 then 'A' 
+    else 'B' end) as `grade`
 from ba.interview_user u
 left join (
   select 
-    ub.user_id, 
-    'A' as `grade`
-  from ba.interview_user_book ub
-  join ba.interview_user u on ub.user_id = u.id 
-    and u.status = 'active' 
-    and substring(u.signup, 1, 7) < '2017-03'
-  group by ub.user_id
-  having sum(ub.price) >= 1000) g on u.id = g.user_id
+    user_id, 
+    sum(price) as price 
+  from interview_payment 
+  where pay_for = 'book'
+  group by user_id) p on u.id = p.user_id
 ;
-
-# Q5) multikey range join, sub query
-# Q4에서 테스트 데이터를 제거해주세요 (tester : from ~ to 기간 동안 테스트 아이디였다는 뜻입니다)﻿
-select
+ 
+# Q5) range join, sub query
+# Q4에서 테스트 데이터를 제거해주세요 (tester : from ~ to 기간 동안 테스트 아이디였다는 뜻입니다)
+select 
   u.id as `user_id`,
   u.signup,
-  if(g.grade = 'A', 'A', 'B') as `grade`
+  u.`status`,
+  p.price as `cummulative_price`,
+  (case 
+    when u.status = 'active' and u.signup < '2017-03-01 00:00:00' and p.price >= 1000 then 'A' 
+    else 'B' end) as `grade`
 from ba.interview_user u
 left join (
   select 
-    ub.user_id, 
-    'A' as `grade`
-  from ba.interview_user_book ub
-  join ba.interview_user u on ub.user_id = u.id 
-    and u.status = 'active' 
-    and substring(u.signup, 1, 7) < '2017-03'
-  where ub.payment_id not in (
-    select distinct p.id 
-    from ba.interview_payment p
-    join ba.interview_tester t on p.user_id = t.user_id and p.pay_date between t.`from` and t.`to`)
-  group by ub.user_id
-  having sum(ub.price) >= 1000) g on u.id = g.user_id
+    p.user_id, 
+    sum(p.price) as price 
+  from interview_payment p
+  left join ba.interview_tester t on p.user_id = t.user_id and p.pay_date between t.`from` and t.`to`
+  where t.user_id is null
+  and p.pay_for = 'book'
+  group by p.user_id) p on u.id = p.user_id
 ;
