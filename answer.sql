3015f3f94de408fe31019a04ef477bb57b648559
Ha, Hun Cheol <hancury@gmail.com>
2018-09-13 11:32:33 +0900

    Update answer.sql

--- answer.sql
+++ answer.sql
@@ -1,5 +1,5 @@
-# Q1) select, where
-# 2017년 2월 이후 가입 고객의 id, status, signup을 표시해주세요
+-- Q1) select, where
+-- 2017년 2월 이후 가입 고객의 id, status, signup을 표시해주세요
 select 
   id, 
   `status`, 
@@ -8,8 +8,8 @@ from ba.interview_user
 where signup >= '2017-02-01 00:00:00'
 ;
  
-# Q2) where, group by, count, distinct
-# 월별 도서구매고객수를 표시해주세요
+-- Q2) where, group by, count, distinct
+-- 월별 도서구매고객수를 표시해주세요
 select 
   year(pay_date), 
   month(pay_date), 
@@ -21,8 +21,8 @@ group by
   month(pay_date)
 ;
  
-# Q3) left join, group by, sum, if
-# 모든 고객별 id, 누적리디캐시충전액, 누적기기구매액을 표시해주세요
+-- Q3) left join, group by, sum, if
+-- 모든 고객별 id, 누적리디캐시충전액, 누적기기구매액을 표시해주세요
 select 
   u.id, 
   sum(if(p.pay_for = 'ridicash', p.price, 0)) as ridicash, 
@@ -32,9 +32,9 @@ left join ba.interview_payment p on u.id = p.user_id
 group by u.id
 ;
  
-# Q4) where, group by, if, left join
-# 고객 status가 'active'이며 2017년 3월 이전 가입하였고 누적도서구매액이 1000원 이상인 경우 grade 'A' 그 외 고객은 grade 'B'
-# 모든 고객에 대해 `user_id`, `signup`, `status`, `cummulative_price`, `grade` 컬럼을 표시해주세요
+-- Q4) where, group by, if, left join
+-- 고객 status가 'active'이며 2017년 3월 이전 가입하였고 누적도서구매액이 1000원 이상인 경우 grade 'A' 그 외 고객은 grade 'B'
+-- 모든 고객에 대해 `user_id`, `signup`, `status`, `cummulative_price`, `grade` 컬럼을 표시해주세요
 select 
   u.id as `user_id`,
   u.signup,
@@ -53,8 +53,8 @@ left join (
   group by user_id) p on u.id = p.user_id
 ;
  
-# Q5) range join, sub query
-# Q4에서 테스트 데이터를 제거해주세요 (tester : from ~ to 기간 동안 테스트 아이디였다는 뜻입니다)
+-- Q5) range join, sub query
+-- Q4에서 테스트 데이터를 제거해주세요 (tester : from ~ to 기간 동안 테스트 아이디였다는 뜻입니다)
 select 
   u.id as `user_id`,
   u.signup,
